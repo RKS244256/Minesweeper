12aebc69510bb6f85b116466290431c4607b20d6
RKS244256 <118144193+RKS244256@users.noreply.github.com>
2025-06-10 13:00:27 -0400

    [200~Added automated pway, uwu :3 fixed bug whewe wandom minye genyewation ://// added an additionyaw minye, added (^³^) >.< comments. (^³^) OwO >.< >.< (Added automated play, fixed bug where random mine generation added an additional mine, added comments.)

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class App {
@@ -14,16 +15,16 @@ public class App {
         int tMines = 0;
         clearScreen();
         while(true){
-            System.out.println("1 | Easy\n2 | Medium\n3 | Hard\n4 | Custom Mode\nSelect difficulty (#):");
-            String d = userIn.nextLine();
-            if(d.matches("[1-4]")){
+            System.out.println("1 | Easy\n2 | Medium\n3 | Hard\n4 | Custom Mode - From File\n5 | Custom Mode\n6 | Automated Play\nSelect difficulty (#):");
+            String d = userIn.nextLine(); // The "difficulty" selected by the player is stored here.
+            if(d.matches("[1-6]")){
                 if(d.equals("1")){
                     tMines = 20;
                 } else if(d.equals("2")){
                     tMines = 40;
                 } else if(d.equals("3")){
                     tMines = 60;
-                } else{
+                } else if(d.equals("4")){
                     tMines = game.plantMinesFromFile();
                     if(tMines == 0){
                         System.out.println("Invalid File - File either does not exist or has invalid formatting. Ensure file exists and consists of valid coordinates separated by commas");
@@ -34,12 +35,129 @@ public class App {
                         continue;
                     }
                     break;
+                } else if(d.equals("5")){
+                    System.out.println("Enter number of mines");
+                    d = userIn.nextLine();
+                    if(d.matches("[0-9]+")){
+                        if(Integer.parseInt(d) > 0 && Integer.parseInt(d) <= 256){
+                            tMines = Integer.parseInt(d);
+                        } else{
+                            System.out.println("Invalid Input - Input should be between 0 and 256");
+                            userIn.nextLine();
+                            continue;
+                        }
+                    } else{
+                        System.out.println("Invalid Input - Input should be an integer");
+                        userIn.nextLine();
+                        continue;
+                    }
+                } else{
+                    tMines = game.plantMinesFromFile();
+                    if(tMines == 0){
+                        System.out.println("Invalid File - File either does not exist or has invalid formatting. Ensure file exists and consists of valid coordinates separated by commas");
+                        try{
+                            new File("custom.txt").createNewFile();
+                        } catch(IOException e){}
+                        userIn.nextLine();
+                        continue;
+                    }
+                    try{
+                        File f = new File("instructions.txt");
+                        Scanner rf = new Scanner(f).useDelimiter("\n");
+                        char c;
+                        char wrc;
+                        int wcc;
+                        while(rf.hasNext()){
+                            String wc = rf.nextLine().toUpperCase();
+                            System.out.println(wc);
+                            wrc = wc.charAt(0);
+                            if(wc.matches("[A-P][1-9](F|U|R)")){
+                                wcc = Integer.parseInt(wc.substring(1,2));
+                                c = wc.charAt(2);
+                            } else if(wc.matches("[A-P](1[0-6])(F|U|R)")){
+                                wcc = Integer.parseInt(wc.substring(1,2));
+                                c = wc.charAt(3);
+                                
+                            } else {
+                                System.out.println("Invalid Input: Skipping...");
+                                userIn.nextLine();
+                                continue;
+                            }
+                            if(c == 'F'){
+                                if(game.getBoard().get(wrc)[wcc].isRevealed()){
+                                    System.out.println("Invalid Input - Tile already revealed");
+                                    userIn.nextLine();
+                                    clearScreen();
+                                    continue;
+                                }
+                                if(game.getBoard().get(wrc)[wcc].isFlagged()){
+                                    System.out.println("Invalid Input - Tile is already flagged");
+                                    userIn.nextLine();
+                                    clearScreen();
+                                    continue;
+                                }
+                                if(flaggedTiles == tMines){
+                                    System.out.println("Invalid Input - Maximum flags planted");
+                                    userIn.nextLine();
+                                    clearScreen();
+                                    continue;
+                                }
+                                game.getBoard().get(wrc)[wcc].setFlagged(true);
+                                flaggedTiles++;
+                                if(game.getBoard().get(wrc)[wcc].isMine()){
+                                    game.setMines(game.getMines() - 1);
+                                }
+                            } else if(c == 'U'){
+                                if(!game.getBoard().get(wrc)[wcc].isFlagged()){
+                                    System.out.println("Invalid Input - Tile already unflagged");
+                                    userIn.nextLine();
+                                    clearScreen();
+                                    continue;
+                                }
+                                game.getBoard().get(wrc)[wcc].setFlagged(false);
+                                flaggedTiles--;
+                                if(game.getBoard().get(wrc)[wcc].isMine()){
+                                    game.setMines(game.getMines() + 1);
+                                }
+                            } else if(c == 'R'){
+                                if(game.getBoard().get(wrc)[wcc].isFlagged()){
+                                    System.out.println("Invalid Input - Tile is flagged");
+                                    userIn.nextLine();
+                                    clearScreen();
+                                    continue;
+                                }
+                                if(game.getBoard().get(wrc)[wcc].isRevealed()){
+                                    System.out.println("Invalid Input - Tile already revealed");
+                                    userIn.nextLine();
+                                    clearScreen();
+                                    continue;
+                                }
+                                game.getBoard().get(wrc)[wcc].setRevealed(true);
+                                if(game.getBoard().get(wrc)[wcc].isMine()){
+                                    game.setDeath(true);
+                                    continue;
+                                }
+                                game.revealAdjacentEmpties(game.getBoard().get(wrc)[wcc]);
+                            }
+                            game.displayBoard();
+                            System.out.println(flaggedTiles + "/" + tMines);
+                            userIn.nextLine();
+                        }
+                        rf.close();
+                    } catch(FileNotFoundException e){
+                        System.out.println("File Not Found: Enter valid instructions in intructions.txt and try again.");
+                        try{
+                            new File("instructions.txt").createNewFile();
+                        } catch(IOException e2){}
+                        continue;
+                    }
+                    break;
                 }
                 game.plantMines(tMines);
                 clearScreen();
                 break;
             } else{
-                System.out.println("Invalid Input - Input should be 1 digit within the range of 1-4");
+                System.out.println("Invalid Input - Input should be 1 digit within the range of 1-6");
                 userIn.nextLine();
                 clearScreen();
             }
--- Board.java
+++ Board.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 
 public class Board {
@@ -28,7 +29,7 @@ public class Board {
     public void plantMines(int m){ // Takes in a specified number of mines.
         ArrayList<String> p = new ArrayList<String>(); //An array of coords of "planted" mines in string form. Ensures the algorithm doesn't plant mines where it already has,
         mines = m;
-        while(p.size() <= m){ // Checks whether the array size is m (ie, there are m mines) and continues to generate random coords to plant mines if not.
+        while(p.size() < m){ // Checks whether the array size is m (ie, there are m mines) and continues to generate random coords to plant mines if not.
             int rr = (int) (Math.random()*16); // Picks a "random row"
             int rc = (int) (Math.random()*16); // Picks a "random column"
             if(!p.contains(rows.charAt(rr) + String.valueOf(rc))){ // Checks if mine already exists at the coord generated
@@ -39,76 +40,76 @@ public class Board {
     }
 
     public int plantMinesFromFile(){
-        int o = 0;
+        int o = 0; // Integer output <- Returns number of mines planted from file.
         try{
-            File f = new File("custom.txt");
-            Scanner rf = new Scanner(f).useDelimiter(",");
+            File f = new File("custom.txt"); //Can be any file but txts are the most recognized for notepad and allows for easier manual editing
+            Scanner rf = new Scanner(f).useDelimiter(","); //Coords should be separated by commas
             while(rf.hasNext()){
                 String wc = rf.next().toUpperCase();
-                if(wc.matches("[A-P]([1-9]|1[0-6])")){
-                    board.get(wc.charAt(0))[Integer.parseInt(wc.substring(1))-1].setMine(true);
-                    o++;
+                if(wc.matches("[A-P]([1-9]|1[0-6])")){ // Checks if a coord is valid through regex
+                    board.get(wc.charAt(0))[Integer.parseInt(wc.substring(1))-1].setMine(true); // Plants a mine if true
+                    o++; // Increments output to represent a new mine being added to the total mines on the board
                 }
             }
             rf.close();
-            mines = o;
+            setMines(o); // Sets the internal counter for mines to o
             
-        } catch(FileNotFoundException e){
+        } catch(FileNotFoundException e){ // Game won't error out and rather, sets the output to 0 which tells the game the file is either missing or invalid as it doesn't have any proper mine coordinates.
             o = 0;
         }
         return o;
     }
-
+    
     public void displayBoard(){
-        StringBuilder o = new StringBuilder();
-        o.append(Colors.cyan + " | 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13|14|15|16|\n" + Colors.reset);
-        for(char c : board.keySet()){
-            o.append(" |——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|\n");
-            StringBuilder wsb = new StringBuilder();
-            wsb.append(Colors.cyan + c + Colors.reset + "|");
-            for(Coord wCoord : board.get(c)){
-                if(wCoord.isRevealed()){
-                    if(adjacentMines(wCoord) == 0){
-                        wsb.append("  |");
+        StringBuilder o = new StringBuilder(); // "Output" string that is to be displayed when answer key is requested
+        o.append(Colors.cyan + " | 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13|14|15|16|\n" + Colors.reset); // Display top row (columns). Hardcoded.
+        for(char c : board.keySet()){ // Iterating through rows. Each key within the hashmap is stored as a "character"
+            o.append(" |——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|\n"); // Hardcoded row separators
+            StringBuilder wsb = new StringBuilder(); // "Working StringBuilder" <- Used to contruct rows then appended to the "Output" StringBuilder
+            wsb.append(Colors.cyan + c + Colors.reset + "|"); // Adds the row letter first
+            for(Coord wCoord : board.get(c)){ // Iterates the "Working Coordinates" from the Coord array of each row in the "board" hashmap
+                if(wCoord.isRevealed()){ // Checks if the coordinate had already been revealed by the player
+                    if(adjacentMines(wCoord) == 0){ // Checks if the mine has 0 adjacent mines
+                        wsb.append("  |"); // Appends empty box if so
                     } else{
-                        wsb.append(Colors.yellow + " " + adjacentMines(wCoord) + Colors.reset + "|");
+                        wsb.append(Colors.yellow + " " + adjacentMines(wCoord) + Colors.reset + "|"); // Appends number of adjacent mines if not.
                     }
-                } else if(wCoord.isFlagged()){
-                    wsb.append(Colors.red + " F" + Colors.reset + "|");
+                } else if(wCoord.isFlagged()){ // Check if the coordinate is flagged if not revealed
+                    wsb.append(Colors.red + " F" + Colors.reset + "|"); // If so, display a red 'F'
                 } else{
-                    wsb.append(" *|");
+                    wsb.append(" *|"); // If not revealed or flagged, still hidden so add a star to the box.
                 }
             }
-            wsb.append("\n");
-            o.append(wsb);
+            wsb.append("\n"); // Append a new line to ensure board is displayed properly
+            o.append(wsb); // Append the working StringBuilder into the overall output.
         }
-        System.out.println(o);
+        System.out.println(o); // Display game board
     }
 
     public void answerKey(){
-        StringBuilder o = new StringBuilder();
-        o.append(Colors.cyan + " | 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13|14|15|16|\n" + Colors.reset);
-        for(char c : board.keySet()){
-            o.append(" |——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|\n");
-            StringBuilder wsb = new StringBuilder();
-            wsb.append(Colors.cyan + c + Colors.reset + "|");
-            for(Coord wCoord : board.get(c)){
-                if(wCoord.isMine()){
+        StringBuilder o = new StringBuilder(); // "Output" string that is to be displayed when answer key is requested
+        o.append(Colors.cyan + " | 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13|14|15|16|\n" + Colors.reset); // Display top row (columns). Hardcoded.
+        for(char c : board.keySet()){ // Iterating through rows. Each key within the hashmap is stored as a "character"
+            o.append(" |——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|——|\n"); // Hardcoded row separators
+            StringBuilder wsb = new StringBuilder(); // "Working StringBuilder" <- Used to contruct rows then appended to the "Output" StringBuilder
+            wsb.append(Colors.cyan + c + Colors.reset + "|"); // Adds the row letter first
+            for(Coord wCoord : board.get(c)){ // Iterates the "Working Coordinates" from the Coord array of each row in the "board" hashmap
+                if(wCoord.isMine()){ // Displays a red "!" if the coordinate is a mine
                     wsb.append(Colors.red + " !" + Colors.reset + "|");
-                } else{
+                } else{ // Displays the number adjacent mines if not.
                     wsb.append(" " + Colors.yellow + Integer.toString(adjacentMines(wCoord)) + Colors.reset + "|");
                 }
             }
-            wsb.append("\n");
-            o.append(wsb);
+            wsb.append("\n"); // Append a new line to ensure board is displayed properly
+            o.append(wsb); // Appends wsb to the output
         }
-        System.out.println(o);
+        System.out.println(o); // Prints the output to console (the game)
     }
 
-    String[] adjacentArrayContructor(char r, int c){//Rewrite possible
-        int wr = rows.indexOf(r);
-        int i = 0;
-        String[] o = new String[8];
+    String[] adjacentArrayContructor(char r, int c){ // Constructs an array of coordinates stored as Strings after taking a coord in the form of a char "row" and int "column"
+        int wr = rows.indexOf(r); // Converts the char row into a integer "Working Row"
+        int i = 0; // Iterator to traverse the array below.
+        String[] o = new String[8]; // Output array. It will be filled with whatever adjacent coordinates in string form. Any other slots are filled with null values.
         if(wr-1 >= 0 && c-1 >= 0){
             o[i] = rows.charAt(wr-1) + String.valueOf(c-1);
             i++;
@@ -144,30 +145,30 @@ public class Board {
         return o;
     }
 
-    int adjacentMines(Coord wc){
-        int o = 0;
-        int i = 0;
-        String[] adjC = wc.getAdj();
-        while(adjC[i] != null){
-            if(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isMine()){
-                o++;
+    int adjacentMines(Coord wc){ // Returns the number of adjacent mines of the coord specified in the argument.
+        int o = 0; // Integer output of the number of adjacent mines
+        int i = 0; // Iterator, iterates through the String array below
+        String[] adjC = wc.getAdj(); // Gets the coordinates in string form from the "adj" property of the coord
+        while(adjC[i] != null){ // All arrays of the "adj" property are 8 strings in length. If a coord does not have 8 adjacent tiles, the array is filled with null values at the end. Hence, the loop will end if when iterating, the program reaches a null value, indicating a lack of further coords to test.
+            if(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isMine()){ // Using String coord, pulls the real coordinate object and checks whether it's a mine using the mine property.
+                o++; // If so, increment the output.
             }
             i++;
-            if(i>7){
+            if(i>7){ // Prevents the iterator from going out of bounds.
                 return o;
             }
         }
         return o;
     }
 
-    public void revealAdjacentEmpties(Coord wc){
+    public void revealAdjacentEmpties(Coord wc){// Updates the revealed property of adjacent tiles with 0 adjacent mines to true. Takes a Coord as the "Working coord"
         int i = 0;
-        String[] adjC = wc.getAdj();
-        while(adjC[i] != null){
-            if(adjacentMines(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))]) == 0 && !board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isRevealed() && !board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isFlagged() && !board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isMine()){
-                board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].setRevealed(true);
-                revealSurroundings(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))]);
-                revealAdjacentEmpties(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))]);
+        String[] adjC = wc.getAdj(); // Retrieves "Adjacent Coords" from "adj" property of the coord
+        while(adjC[i] != null){ // All arrays of the "adj" property are 8 strings in length. If a coord does not have 8 adjacent tiles, the array is filled with null values at the end. Hence, the loop will end if when iterating, the program reaches a null value, indicating a lack of further coords to test.
+            if(adjacentMines(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))]) == 0 && !board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isRevealed() && !board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isFlagged() && !board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].isMine()){ // Checks whether specified coordinates are not mines, already revealed, or flagged AND has 0 adjacent mines.
+                board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))].setRevealed(true); // Reveals the tile.
+                revealSurroundings(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))]); // Reveals the surrounding tiles.
+                revealAdjacentEmpties(board.get(adjC[i].charAt(0))[Integer.parseInt(adjC[i].substring(1))]); // Recursive call to further reveal tiles with 0 adjacent mines.
             }
             i++;
             if(i>7){
